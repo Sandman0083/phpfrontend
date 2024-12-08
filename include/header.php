62d6dafdf1714df0b8d7a1e67036970572b3b505
Sandman0083 <82945200+Sandman0083@users.noreply.github.com>
2024-12-08 17:14:24 +0530

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,44 @@
+	<div class="topbar">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="logo">
+						<img src="images/logo.jpg">
+						
+					</div>
+					
+				</div>
+
+				<div class="col-md-8">
+					<div class="menu">
+						<ul>
+							<li><a href="index.php">home</a></li>
+							<li><a href="about.php">about us</a></li>
+							<li><a href="course.php">course</a></li>
+							<li><a href="gallery.php">gallery</a></li>
+								<li><a href="online.php">online class</a></li>
+						</ul>
+						
+					</div>
+					
+				</div>
+
+
+				
+			</div>
+			
+		</div>
+		
+	</div>
+	
+
+
+				
+
+
+
+
+
+
+</body>
+</html>
